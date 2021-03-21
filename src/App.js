45fd83a1bd125046be57454716b72590266c712b
Neo <speldipn@gmail.com>
2021-03-21 14:06:01 +0900

    MyComponent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import React from "react";
-import PureComponent from "./PureComponent";
 
-class App extends React.Component {
+class MyComponent extends React.Component {
+  componentDidUpdate() {
+    console.log("MyComponent updated");
+  }
   render() {
     return (
       <div>
-        <PureComponent />
+        <h1>MyComponent</h1>
       </div>
     );
   }
 }
 
-export default App;
+class MyPureComponent extends React.Component {
+  componentDidUpdate() {
+    console.log("MyPureComponent updated");
+  }
+  render() {
+    return (
+      <div>
+        <h1>MyPureComponent</h1>
+      </div>
+    );
+  }
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.listValue = [{ name: "Park" }, { name: "Lee" }];
+    this.state = { version: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    setTimeout(() => {
+      this.listValue[0].name = "Justin";
+      this.setState({ version: 1 });
+    }, 100);
+    setTimeout(() => {
+      this.listValue = [{ name: "Justin" }, { name: "Lee" }];
+      this.setState({ version: 2 });
+    }, 200);
+  }
+
+  render() {
+    return (
+      <div className="body">
+        <MyComponent value={this.listValue} />
+        <MyPureComponent value={this.listValue} />
+        <button onClick={this.handleClick}>Button</button>
+      </div>
+    );
+  }
+}
---
+++ src/MyComponent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+  componentDidUpdate() {
+    console.log("MyComponent updated");
+  }
+}
